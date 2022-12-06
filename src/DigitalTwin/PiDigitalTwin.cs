fe8239a8cb72db7236ec7ce4a1116681292ccd85
eggebee6 <54823750+eggebee6@users.noreply.github.com>
2022-12-06 12:59:45 -0600

    Add DigitalTwin

---
+++ src/DigitalTwin/PiDigitalTwin.cs
@@ -0,0 +1,29 @@
+﻿using DigitalTwin.Comms;
+using DigitalTwin.Physical;
+using DigitalTwin.Virtual;
+using DigitalTwin.Comms.Ua;
+using Opc.Ua.Server;
+using Opc.Ua;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitalTwin
+{
+  public class PiDigitalTwin
+  {
+    public readonly IPhysicalTwin PhysicalTwin;
+    public readonly VirtualTwin VirtualTwin;
+    public readonly UaServer UaServer;
+
+    public PiDigitalTwin(
+      IPhysicalTwin physicalTwin,
+      UaServer uaServer)
+    {
+      PhysicalTwin = physicalTwin ?? throw new ArgumentNullException(nameof(physicalTwin));
+      UaServer = uaServer ?? throw new ArgumentNullException(nameof(uaServer));
+
+      VirtualTwin = new VirtualTwin(PhysicalTwin);
+    }
+  }
+}
